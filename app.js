908472bf14ffe0e168faec2a9106136d7b0fd73a
RadmiloM <markovicradmilo35@gmail.com>
2024-11-02 19:27:08 +0100

    create time function and main function with incrementing possibilities

---
+++ app.js
@@ -0,0 +1,38 @@
+let currentTime = document.querySelector('.current-time');
+
+
+function getCurrentTime() {
+
+    const currentDate = new Date();
+    let hours = currentDate.getHours();
+    let  minutes = currentDate.getMinutes();
+    let seconds = currentDate.getSeconds();
+    const noon = 12;
+    let meridian = 'AM';
+
+    if(hours > noon) {
+        hours-= noon;
+        meridian = 'PM';
+    }
+
+    if(minutes < 10) {
+        minutes = `0${minutes}`;
+    }
+
+    if(seconds < 10) {
+        seconds = `0${seconds}`
+    }
+
+    let time = `${hours}:${minutes}:${seconds} ${meridian}!`;
+
+    currentTime.textContent = time;
+
+}
+
+
+function main() {
+    let updateSeconds = 1000;
+    setInterval(getCurrentTime, updateSeconds);
+}
+
+main();
